d8bc1c83fa72a0f2f5bb7c842f250b7b5fc02dbb
gualdras <victor.buke@gmail.com>
2016-02-15 08:52:40 +0100

    Not working version

---
+++ itemTypes.py
@@ -0,0 +1,21 @@
+from google.appengine.ext import ndb
+
+class User(ndb.Model):
+    phoneNumber = ndb.IntegerProperty()
+    regID = ndb.StringProperty()
+    
+    
+    
+    def user2json(self):
+        return {"user":self.key.id()}
+        
+    def userComplete2json(self):
+		uDetails = {"phoneNumber": self.key.id(), "regID": self.regID}
+		return uDetails
+        
+    @classmethod
+    def getAll(self):
+        auxList = []
+        for item in User.query():
+            auxList.append(item.user2json())
+        return auxList
